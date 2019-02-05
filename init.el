ab19a366b75863a1d8e68d189f817b65406cfafa
toyama <toyama.jouji@gmail.com>
2019-02-05 17:55:50 +0900

    upgrade emacs init

--- init.el
+++ init.el
@@ -9,7 +9,8 @@
 (cask-initialize)
 (require 'pallet)
 (pallet-mode t)
-(global-auto-complete-mode t)
+;;(global-auto-complete-mode t)
+(require 'python-mode)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -18,7 +19,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell drag-stuff auto-complete))))
+    (jedi py-autopep8 yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell drag-stuff))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -26,7 +27,20 @@
  ;; If there is more than one, they won't work right.
  )
 
-(global-auto-complete-mode t)
+;;(jedi:setup)
+;;  (define-key jedi-mode-map (kbd "<C-tab>") nil) ;;C-tabはウィンドウの移動に用いる
+;;  (setq jedi:complete-on-dot t)
+;;  (setq ac-sources
+;;    (delete 'ac-source-words-in-same-mode-buffers ac-sources)) ;;jediの補完候補だけでいい
+;;  (add-to-list 'ac-sources 'ac-source-filename)
+;;  (add-to-list 'ac-sources 'ac-source-jedi-direct)
+;;  (define-key python-mode-map "\C-ct" 'jedi:goto-definition)
+;;  (define-key python-mode-map "\C-cb" 'jedi:goto-definition-pop-marker)
+;;  (define-key python-mode-map "\C-cr" 'helm-jedi-related-names)
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
+;; (global-auto-complete-mode t)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq auto-save-list-file-name nil)
@@ -59,3 +73,8 @@
 
 (setq ring-bell-function 'ignore)
 (put 'upcase-region 'disabled nil)
+
+(setq hl-line-face 'underline)
+(global-hl-line-mode)
+
+(load-theme 'atom-one-dark t)
